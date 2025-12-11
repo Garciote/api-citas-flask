c8923c477b5346206afcfd4a517a4719f60767d3
Garciote <245249699+Garciote@users.noreply.github.com>
2025-12-11 23:20:57 +0100

    dot env directory specification in wsgi

--- wsgi.py
+++ wsgi.py
@@ -6,14 +6,10 @@ import sys
 # For some reason we need to set the current directory to this otherwise dotenv doesn't work correctly.
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Currently setting environment variable fallbacks here.
-# They should be set by the .env file
-os.environ.setdefault("MONGODB_URI", "")
-os.environ.setdefault("JWT_SECRET_KEY", "")
-
 # Load .env variables.
 from dotenv import load_dotenv
-load_dotenv(override=True)
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path, override=True)
 
 # From application(.py) import app (as _app)
 from application import app as _app
